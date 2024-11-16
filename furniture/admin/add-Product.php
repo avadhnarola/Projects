d5b116c02b1fe388e08c44fa78233b8ec28e6023
avadh <avadhnarola2515@gmail.com>
2024-11-16 18:23:56 +0530

    view:

--- furniture/admin/add-Product.php
+++ furniture/admin/add-Product.php
@@ -19,50 +19,52 @@ if (isset($_POST['submit'])) {
     header("location:add-Product.php");
 }
 ?>
+<div class="content-wrapper">
 
-<div class="mt-5">
-    <div class="col-xl-6 col-md-12 m-auto mt-5">
-        <div class="card">
-            <div class="card-content">
-                <div class="card-body">
+    <div class="mt-5">
+        <div class="col-xl-6 col-md-12 m-auto mt-5">
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-body">
 
-                    <form class="form" method="post" enctype="multipart/form-data">
-                        <div class="form-body">
-                            <h3 class="text-center">Product Form</h3>
+                        <form class="form" method="post" enctype="multipart/form-data">
+                            <div class="form-body">
+                                <h3 class="text-center">Product Form</h3>
 
-                            <div class="form-group mt-2">
-                                <label for="donationinput3" class="sr-only mb-1 mt-4">Name</label>
-                                <input type="text" name="name" id="donationinput3" class="form-control"
-                                    placeholder="Name.." required>
-                            </div>
+                                <div class="form-group mt-2">
+                                    <label for="donationinput3" class="sr-only mb-1 mt-4">Name</label>
+                                    <input type="text" name="name" id="donationinput3" class="form-control"
+                                        placeholder="Name.." required>
+                                </div>
 
-                            <div class="form-group">
-                                <label for="donationinput4" class="sr-only  mb-1 mt-4">Amount</label>
-                                <input type="text" name="amount" id="donationinput4" class="form-control"
-                                    placeholder="Amount" required>
-                            </div>
+                                <div class="form-group">
+                                    <label for="donationinput4" class="sr-only  mb-1 mt-4">Amount</label>
+                                    <input type="text" name="amount" id="donationinput4" class="form-control"
+                                        placeholder="Amount" required>
+                                </div>
 
-                            <div class="form-group">
-                                <label for="donationinput4" class="sr-only  mb-1 mt-4">Product Image</label>
-                                <input type="file" name="image" id="donationinput4" class="form-control" required>
-                            </div>
+                                <div class="form-group">
+                                    <label for="donationinput4" class="sr-only  mb-1 mt-4">Product Image</label>
+                                    <input type="file" name="image" id="donationinput4" class="form-control" required>
+                                </div>
 
 
 
-                            <div class="center d-flex mt-5" style="justify-content: center">
-                                <button type="submit" name="submit" class="btn btn-outline-primary mt-1">Submit</button>
-                            </div>
-                    </form>
+                                <div class="center d-flex mt-5" style="justify-content: center">
+                                    <button type="submit" name="submit"
+                                        class="btn btn-outline-primary mt-1">Submit</button>
+                                </div>
+                        </form>
 
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
-
 </div>
 
-<div class="pt-5 mt-5">
-    <?php
-    include_once 'footer.php';
-    ?>
-</div>
+
+<?php
+include_once 'footer.php';
+?>
